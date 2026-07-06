88d2e372f06ad6d34f2a798b6cc12ef53f4eabe4
Zed1118 <1050613234@qq.com>
2026-07-06 15:17:38 +0800

    浅底 StageProgressRow 下一阶金值统一传 jiang(藏经阁武学库+武学详情)

--- lib/features/cangjingge/presentation/skill_proficiency_row.dart
+++ lib/features/cangjingge/presentation/skill_proficiency_row.dart
@@ -3,6 +3,7 @@ import 'package:wuxia_idle/data/defs/skill_def.dart';
 import 'package:wuxia_idle/data/numbers_config.dart';
 import 'package:wuxia_idle/features/cultivation/application/skill_proficiency_formatter.dart';
 import 'package:wuxia_idle/shared/strings.dart';
+import 'package:wuxia_idle/shared/theme/wuxia_tokens.dart';
 import 'package:wuxia_idle/shared/widgets/wuxia_ui/stage_progress_row.dart';
 
 /// 招式熟练度展示行（藏经阁主屏 · P1b Task7）。
@@ -52,6 +53,8 @@ class SkillProficiencyRow extends StatelessWidget {
       ratio: summary.ratio,
       currentEffect: summary.currentEffect,
       nextEffect: summary.nextEffect,
+      // 藏经阁武学库在浅宣纸底(PaperPanel):下一阶用绛红(默认金叠浅纸糊)。
+      nextEffectColor: WuxiaUi.jiang,
       progressText: summary.progressText,
       tag: equipped ? UiStrings.cangjingEquippedTag : null,
       tagHighlighted: true,
